edf59b7c53e1649fb7fe8ac709944edcc5ef112c
Lukease <lukpawlowski@gmail.com>
2022-04-08 15:02:56 +0200

    initial commit

---
+++ vite-project/main.js
@@ -0,0 +1,9 @@
+const app = document.querySelector('main')
+
+const ticTacBox = document.createElement('div')
+ticTacBox.classList.add('box')
+app.appendChild(ticTacBox)
+
+const ticTacArena = document.createElement('div')
+ticTacArena.classList.add('arena')
+app.appendChild(ticTacArena)
