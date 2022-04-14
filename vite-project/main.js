1521b7dd0362f69cfdd2275176e3931a60fb51a2
Lukease <lukpawlowski@gmail.com>
2022-04-14 11:22:00 +0200

    add new function whoWin().

--- vite-project/main.js
+++ vite-project/main.js
@@ -43,6 +43,22 @@ const createTicTacArena = () => {
     return ticTacArena
 }
 
+let whoWin = () =>{
+    const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    const xWin = win.find(result => result.every(index => cubeArray[index] === 'X'))
+    const oWin = win.find(result => result.every(index => cubeArray[index] === 'O'))
+
+    if (xWin) {
+        alert('X win the game')
+        window.location.reload()
+    }
+
+    if (oWin) {
+        alert('O win the game')
+        window.location.reload()
+    }
+}
+
 const ticTacBox = createTicTacChoice()
 const arena = createTicTacArena()
 
@@ -54,28 +70,38 @@ cubeArray.forEach((object, index) => {
     newCube.classList.add('cube')
     arena.appendChild(newCube)
     newCube.addEventListener('click', () => {
-
         const choice = document.querySelector('.app__box--choice.choice-green').firstChild.classList.value
-        if (newCube.classList.contains('cube') && choice === 'circle' && !newCube.hasChildNodes()) {
+
+        if(!newCube.hasChildNodes() && !newCube.classList.contains('cube')) {
+            return
+        }
+
+        if (choice === 'circle') {
             const myChoice = document.createElement('div')
 
             myChoice.classList.add('circle')
             newCube.appendChild(myChoice)
             arena.style.pointerEvents = 'none'
             cubeArray[index] = 'O'
-        } if (newCube.classList.contains('cube') && choice === 'cross' && !newCube.hasChildNodes()) {
-            const firstCross = document.createElement('div')
 
-            firstCross.classList.add('cross')
-            newCube.appendChild(firstCross)
+            whoWin()
 
-            const secondCross = document.createElement('div')
-
-            secondCross.classList.add('cross')
-            firstCross.appendChild(secondCross)
-            arena.style.pointerEvents = 'none'
-            cubeArray[index] = 'X'
+            return
         }
+
+        const firstCross = document.createElement('div')
+
+        firstCross.classList.add('cross')
+        newCube.appendChild(firstCross)
+
+        const secondCross = document.createElement('div')
+
+        secondCross.classList.add('cross')
+        firstCross.appendChild(secondCross)
+        arena.style.pointerEvents = 'none'
+        cubeArray[index] = 'X'
+
+        whoWin()
     })
 })
 
@@ -102,9 +128,9 @@ const play = () => {
     ticTacBox.removeEventListener('click', makeChoice)
 
     const choice = document.querySelector('.app__box--choice.choice-green').firstChild.classList.value
-
-    const indexes = cubeArray.map((field, index) => field ? null : index)
-    const filtered = indexes.filter(item => item !== null)
+    const filtered = cubeArray
+        .map((field, index) => field ? null : index)
+        .filter(item => item !== null)
     const randomNumber = Math.floor(Math.random() * filtered.length)
     const randomNumberValue = filtered[randomNumber]
 
@@ -126,33 +152,25 @@ const play = () => {
             firstCross.appendChild(secondCross)
             arena.style.pointerEvents = 'auto'
             cubeArray[randomNumberValue] = 'X'
+
+            whoWin()
         }, 1000)
     }
 
     if (allCube[randomNumberValue].classList.contains('cube') && !allCube[randomNumberValue].hasChildNodes() && choice === 'cross') {
         setTimeout(() => {
             const myChoice = document.createElement('div')
-            
+
             myChoice.classList.add('circle')
             allCube[randomNumberValue].appendChild(myChoice)
             arena.style.pointerEvents = 'auto'
             cubeArray[randomNumberValue] = 'O'
+            
+            whoWin()
         }, 1000)
-    }
-    const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-
-    const xWin = win.find(result => result.every(index => cubeArray[index] === 'X'))
-    const oWin = win.find(result => result.every(index => cubeArray[index] === 'O'))
-
-    if (xWin) {
-        alert('X win the game')
-        window.location.reload()
-    }
-
-    if (oWin !== undefined) {
-        alert('O win the game')
-        window.location.reload()
     }
 }
 
 arena.addEventListener('click', play)
+
+
